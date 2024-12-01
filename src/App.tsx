d473917fed193c17afed6879164bc60340e52b0a
tarakish <akr.3838soccer14@gmail.com>
2024-12-01 14:24:54 +0900

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Button
-        </button>
+        <Button>Click me</Button>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
